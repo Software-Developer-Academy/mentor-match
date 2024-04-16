f213c054461af5b53b70d1ae12990ea809a6bf34
harunugurlu <harunugurlu1@gmail.com>
2024-04-16 19:32:15 +0300

    fix fullName input min max length issue

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -15,8 +15,8 @@ const SignUp = () => {
 
   const signUpSchema = z.object({
     fullName: z.string()
-      .min(A, { message: `Name must be at least ${A} characters long` })
-      .max(B, { message: `Name must be no more than ${B} characters long` })
+      .min(MIN_NAME_LENGTH, { message: `Name must be at least ${MIN_NAME_LENGTH} characters long` })
+      .max(MAX_NAME_LENGTH, { message: `Name must be no more than ${MAX_NAME_LENGTH} characters long` })
       .regex(/^[a-zA-Z'-]+$/, { message: "Name can only include alphabetical characters, hyphens, and apostrophes" }),
     email: z.string()
       .email({ message: "Please enter a valid email" }),
