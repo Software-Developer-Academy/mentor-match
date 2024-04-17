6cf8a24cc9f42f2d43ed9716a65bf2caa800edf4
Colton Ehrman <coltonje95@gmail.com>
2024-04-17 09:18:45 -0500

    chore: cleanup comment

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,11 +5,13 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const passwordRegex =
-  // Add for a wider range of special characters /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}:"<>?;',.\/`~\-=\[\]\\|])[A-Za-z\d!@#$%^&*()_+{}:"<>?;',.\/`~\-=\[\]\\|]{8,}$/
+/*
+  Add for a wider range of special characters /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}:"<>?;',.\/`~\-=\[\]\\|])[A-Za-z\d!@#$%^&*()_+{}:"<>?;',.\/`~\-=\[\]\\|]{8,}$/
 
-  //   One lowercase letter [a-z]
-  // One uppercase letter [A-Z]
-  // One digit \d
-  //One special character from the set [@$!%*?&.]
+  - One lowercase letter [a-z]
+  - One uppercase letter [A-Z]
+  - One digit \d
+  -One special character from the set [@$!%*?&.]
+*/
+export const passwordRegex =
   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.])[A-Za-z\d@$!%*?&.]{8,}$/;
