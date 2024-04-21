adf2c49a7c60bd2c7c13b1710f980df8a1e9a568
Tahnee McCoy <tahnee.yquin@gmail.com>
2024-04-21 11:56:14 -0700

    layout.tsx format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${poppins.variable} ${andada_pro.variable}`}>
-      <body className={inter.className}>
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
