8ac5c815956880336b1f2d798e4d76e60dd45dae
Colton Ehrman <coltonje95@gmail.com>
2024-04-22 19:58:38 -0500

    feat: modify hero section on /mentors

--- src/app/mentors/page.tsx
+++ src/app/mentors/page.tsx
@@ -7,11 +7,12 @@ export default function Mentors() {
     <>
       <Navbar />
       <main className="pt-[56px] container">
-        <div className="flex items-center justify-center h-[400px] bg-center bg-cover bg-[url('https://images.unsplash.com/photo-1476820865390-c52aeebb9891?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]">
+        <div className="flex flex-col items-center justify-center mt-4">
+          <h1>Find your mentor</h1>
           <input
             type="text"
             placeholder="Search for mentors"
-            className="w-1/2 bg-gray-300 text-black size-12 border px-4 rounded"
+            className="my-6 w-1/2 bg-gray-300 text-black size-12 border px-4 rounded"
           />
         </div>
       </main>
