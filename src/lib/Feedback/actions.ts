0eb6793ac7f3378b607c518bffd987306d9bb9f9
Ramzi-Abidi <ramziinfo2001@gmail.com>
2024-05-07 19:36:25 +0100

    add feedback model, action and validators

--- src/lib/Feedback/actions.ts
+++ src/lib/Feedback/actions.ts
@@ -1,49 +1,44 @@
 "use server";
 
-import bcrypt from "bcrypt";
-import { redirect } from "next/navigation";
 import { ZodIssue } from "zod";
 
 import { connectMongo } from "../db";
-import { createSession, setSessionCookie } from "../utils/session";
-
-import UserModel from "./model";
 
 import FeedbackModel from "./model";
 import { feedbackSchema } from "./validations";
 
 export async function submitFeedback(
-    data: FormData,
+  data: FormData,
 ): Promise<ZodIssue[] | unknown> {
-    const userId = data.get("userId");
-    const mentorId = data.get("mentorId");
-    const rating = data.get("rating");
-    const comment = data.get("comment");
+  const userId = data.get("userId");
+  const mentorId = data.get("mentorId");
+  const rating = data.get("rating");
+  const comment = data.get("comment");
 
-    const dataSchemaValidation = feedbackSchema.safeParse({
-        userId,
-        mentorId,
-        rating,
-        comment,
+  const dataSchemaValidation = feedbackSchema.safeParse({
+    userId,
+    mentorId,
+    rating,
+    comment,
+  });
+
+  if (dataSchemaValidation.success === false) {
+    return dataSchemaValidation.error.errors;
+  }
+
+  await connectMongo();
+
+  try {
+    await FeedbackModel.create({
+      userId: userId,
+      mentorId: mentorId,
+      comment: comment,
+      rating: rating,
     });
-
-    if (dataSchemaValidation.success === false) {
-        return dataSchemaValidation.error.errors;
-    }
-
-    await connectMongo();
-
-    try {
-        await FeedbackModel.create({
-            userId: userId,
-            mentorId: mentorId,
-            comment: comment,
-            rating: rating,
-        });
-    } catch (error) {
-        console.error(error);
-        throw new Error(
-            "We encountered a problem creating feedback. Please try again.",
-        );
-    }
+  } catch (error) {
+    console.error(error);
+    throw new Error(
+      "We encountered a problem creating feedback. Please try again.",
+    );
+  }
 }
