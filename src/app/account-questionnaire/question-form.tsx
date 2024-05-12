6ff3c7b5ce5305f543b966dc60775def865626d0
Tahnee McCoy <tahnee.yquin@gmail.com>
2024-05-12 01:18:04 -0700

    Style form based on UI design

--- src/app/account-questionnaire/question-form.tsx
+++ src/app/account-questionnaire/question-form.tsx
@@ -30,6 +30,14 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselDots,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 import { signupQuestions } from "@/data/signup-questions"
 
@@ -58,108 +66,127 @@ const QuestionForm = () => {
   const onSubmit = (values: z.infer<typeof formSchema>) => console.log(JSON.stringify(values));
 
   return (
-    <Form {...form}>
-      <form
-        className="flex flex-col gap-6 w-full"
-        ref={formRef}
-        onSubmit={form.handleSubmit(onSubmit)}
-        noValidate
-      >
-        {signupQuestions.map((question, index) => (
-          <FormField 
-            key={index}
-            control={form.control}
-            name={question.id as "q1" | "q2" | "q3" | "q4" | "q5"}
-            render={({ field }) => (
-              <FormItem>
-                <div>
-                  <FormLabel>{question.question}</FormLabel>
-                  <FormDescription>Description</FormDescription>
-                </div>
-                <FormControl>
-                  <>
-                    {question.type === "radio" && (
-                      <RadioGroup
-                        onValueChange={field.onChange}
-                      >
-                        {question.options.map((option, index) => (
-                          <FormItem key={index}>
-                            <FormControl>
-                              <RadioGroupItem value={'value' in option ? option.value : ""} />
-                            </FormControl>
-                            <FormLabel>
-                              {'value' in option ? option.value : ""}
-                            </FormLabel>
-                          </FormItem>
-                        ))}
-                      </RadioGroup>
-                    )}
-                    {question.type === "checkbox" && (
-                      <>
-                        {question.options.map((option, index) => (
-                          <div key={index}>
-                            <div>
-                              <Label>{'title' in option ? option.title : ""}</Label>
-                            </div>
-                            {'options' in option ? option.options.map((item, index) => (
-                              <FormField
-                                key={index}
-                                control={form.control}
-                                name={question.id as "q1" | "q2" | "q3" | "q4" | "q5"}
-                                render={({ field }) => {
-                                  return (
-                                    <FormItem key={index}>
-                                      <FormControl>
-                                        <Checkbox
-                                          {...field}
-                                          name={question.id}
-                                          value={'value' in item ? item.value : ""}
-                                          checked={Array.isArray(field.value) ? field.value.includes(item.value) : false}
-                                          onCheckedChange={(checked) => {
-                                            return checked
-                                              ? field.onChange([...(Array.isArray(field.value) ? field.value : []), item.value])
-                                              : field.onChange(Array.isArray(field.value) ? field.value.filter((value) => value !== item.value) : [])
-                                          }}
-                                        />
-                                      </FormControl>
-                                      <FormLabel>
-                                        {'value' in item ? item.value : ""}
-                                      </FormLabel>
-                                    </FormItem>
-                                  )
-                                }}
-                              />
-                            )) : ""}
-                          </div>
-                        ))}
-                      </>
-                    )}
-                    {question.type === "select" && (
-                      <Select onValueChange={field.onChange}>
+    <div className="w-full max-h-screen p-5">
+      <Carousel className="pb-5">
+        <Form {...form}>
+          <form
+            className="flex flex-col gap-6 w-full"
+            ref={formRef}
+            onSubmit={form.handleSubmit(onSubmit)}
+            noValidate
+          >
+            <CarouselContent className="max-h-[75dvh]">
+              {signupQuestions.map((question, index) => (
+                <CarouselItem key={index} className="overflow-y-auto flex justify-center">
+                  <FormField
+                    control={form.control}
+                    name={question.id as "q1" | "q2" | "q3" | "q4" | "q5"}
+                    render={({ field }) => (
+                      <FormItem className="p-5 w-1/2 my-auto">
+                        <div>
+                          <FormLabel><h5 className="text-2xl font-normal">{question.question}</h5></FormLabel>
+                          <FormDescription className="mb-10">Description</FormDescription>
+                        </div>
                         <FormControl>
-                          <SelectTrigger>
-                            <SelectValue placeholder="Select your preferred language" />
-                          </SelectTrigger>
+                          <>
+                            {question.type === "radio" && (
+                              <RadioGroup
+                                onValueChange={field.onChange}
+                              >
+                                {question.options.map((option, index) => (
+                                  <FormItem 
+                                    key={index} 
+                                    className="bg-[#d9d9d9] rounded-md transition-colors duration-300 hover:bg-secondary hover:text-black checked:bg-secondary checked:text-black checked:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-secondary focus:ring-offset-white overflow-hidden"
+                                  >
+                                    <FormControl>
+                                      <RadioGroupItem value={'value' in option ? option.value : ""} className="peer hidden"/>
+                                    </FormControl>
+                                    <FormLabel
+                                      className="!mt-0 py-3 px-4 w-full block text-black cursor-pointer peer-checked:bg-secondary peer-checked:text-black peer-checked:border-transparent peer-aria-checked:bg-secondary peer-aria-checked:text-black peer-aria-checked:border-transparent"
+                                    >
+                                      {'value' in option ? option.value : ""}
+                                    </FormLabel>
+                                  </FormItem>
+                                ))}
+                              </RadioGroup>
+                            )}
+                            {question.type === "checkbox" && (
+                              <div className="grid grid-cols-1 gap-5">
+                                {question.options.map((option, index) => (
+                                  <div key={index}>
+                                    <div>
+                                      <Label>{'title' in option ? option.title : ""}</Label>
+                                    </div>
+                                    {'options' in option ? option.options.map((item, index) => (
+                                      <FormField
+                                        key={index}
+                                        control={form.control}
+                                        name={question.id as "q1" | "q2" | "q3" | "q4" | "q5"}
+                                        render={({ field }) => {
+                                          return (
+                                            <FormItem 
+                                              key={index}
+                                              className="inline-flex m-2 ml-0 bg-[#d9d9d9] text-black rounded-md cursor-pointer transition-colors duration-300 hover:bg-secondary hover:text-black checked:bg-secondary checked:text-black checked:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-secondary focus:ring-offset-white overflow-hidden"
+                                            >
+                                              <FormControl>
+                                                <Checkbox
+                                                  {...field}
+                                                  name={question.id}
+                                                  value={'value' in item ? item.value : ""}
+                                                  checked={Array.isArray(field.value) ? field.value.includes(item.value) : false}
+                                                  onCheckedChange={(checked) => {
+                                                    return checked
+                                                      ? field.onChange([...(Array.isArray(field.value) ? field.value : []), item.value])
+                                                      : field.onChange(Array.isArray(field.value) ? field.value.filter((value) => value !== item.value) : [])
+                                                  }}
+                                                  className="hidden"
+                                                />
+                                              </FormControl>
+                                              <FormLabel className="!mt-0 py-2 px-4 peer-aria-checked:bg-secondary peer-aria-checked:text-black peer-aria-checked:border-transparent peer-checked:bg-secondary peer-checked:text-black peer-checked:border-transparent cursor-pointer">
+                                                {'value' in item ? item.value : ""}
+                                              </FormLabel>
+                                            </FormItem>
+                                          )
+                                        }}
+                                      />
+                                    )) : ""}
+                                  </div>
+                                ))}
+                              </div>
+                            )}
+                            {question.type === "select" && (
+                              <Select onValueChange={field.onChange}>
+                                <FormControl>
+                                  <SelectTrigger>
+                                    <SelectValue placeholder="Select your preferred language" />
+                                  </SelectTrigger>
+                                </FormControl>
+                                <SelectContent>
+                                  {question.options.map((option, index) => (
+                                    <SelectItem key={index} value={'value' in option ? option.value : ""}>
+                                      {'value' in option ? option.value : ""}
+                                    </SelectItem>
+                                  ))}
+                                </SelectContent>
+                              </Select>
+                            )}
+                          </>
                         </FormControl>
-                        <SelectContent>
-                          {question.options.map((option, index) => (
-                            <SelectItem key={index} value={'value' in option ? option.value : ""}>
-                              {'value' in option ? option.value : ""}
-                            </SelectItem>
-                          ))}
-                        </SelectContent>
-                      </Select>
+                        <FormMessage />
+                      </FormItem>
                     )}
-                  </>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        ))}
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+                  />
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+        <CarouselDots className="mt-5" />
+        <CarouselPrevious className="fill-slate-300" />
+        <CarouselNext />
+      </Carousel>
+    </div>
   )
 }
 
